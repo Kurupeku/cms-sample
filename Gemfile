23b8c79c7cdf8380910c50e690c5213c02968cb5
Kurupeku <y.u.kurupeku@gmail.com>
2020-11-20 12:39:44 +0000

    rspec intialized

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -67,6 +68,7 @@ end
 
 group :test do
   gem 'rspec-rails'
+  gem 'webdrivers'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -20,6 +22,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module App
+  # Application config
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
@@ -31,5 +34,44 @@ module App
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Timezone config
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :utc
+
+    # I18n
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    # Permit cross origin
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 expose: %w[access-token expiry token-type uid client],
+                 methods: %i[get post options head]
+      end
+    end
+
+    # Rspec config
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       model_specs: true,
+                       request_specs: true,
+                       system_specs: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
+
+    # CSRF disable
+    config.action_controller.default_protect_from_forgery = true
+
+    # Security for dos attack
+    config.middleware.use Rack::Attack
   end
 end
