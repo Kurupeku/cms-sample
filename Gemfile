3ea6c07bcc86b6582d311fc4a28826ecd0a3590d
Kurupeku <y.u.kurupeku@gmail.com>
2020-11-29 14:10:09 +0000

    set up webpacker

--- Gemfile
+++ Gemfile
@@ -69,6 +69,8 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'annotate'
+  # To use live compilation
+  gem 'foreman'
 end
 
 group :test do
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def check_active(target_path, exactly: false)
+    if exactly
+      target_path == request.path ? 'active' : ''
+    else
+      Regexp.new(target_path) =~ request.path ? 'active' : ''
+    end
+  end
+
+  def check_uk_active(target_path, exactly: false)
+    if exactly
+      target_path == request.path ? 'uk-active' : ''
+    else
+      Regexp.new(target_path) =~ request.path ? 'uk-active' : ''
+    end
+  end
 end
